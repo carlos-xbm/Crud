75c74012222b396f5673dc6ef0b7e363a3b77b7e
carlos-xbm <carlos.xbm@outlook.com>
2025-03-25 20:03:35 -0300

    Codigo para paginaçao e busca adicionado

--- Crud/wwwroot/js/site.js
+++ Crud/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let table = new DataTable('#myTable', {
+    language: {
+        emptyTable: "Nenhum registro encontrado na tabela",
+        info: "Mostrar _START_ até _END_ de _TOTAL_ registros",
+        infoEmpty: "Mostrar 0 até 0 de 0 registros",
+        infoFiltered: "(Filtrar de _MAX_ total de registros)",
+        thousands: ".",
+        lengthMenu: "Mostrar _MENU_ registros por página",
+        loadingRecords: "Carregando...",
+        processing: "Processando...",
+        zeroRecords: "Nenhum registro encontrado",
+        search: "Pesquisar:",
+        paginate: {
+            next: "Próximo",
+            previous: "Anterior",
+            first: "Primeiro",
+            last: "Último"
+        },
+        aria: {
+            sortAscending: ": Ordenar colunas de forma ascendente",
+            sortDescending: ": Ordenar colunas de forma descendente"
+        }
+    }
+});
+
